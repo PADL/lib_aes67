89fcfbd198c345ec772ecb514dbc4071149d9b5c
Luke Howard <lukeh@padl.com>
2025-10-22 16:32:37 -0400

    move aes67_rtp_buffers.c to aes67_xfifo.c

--- lib_aes67/src/rtp/aes67_xfifo.c
+++ lib_aes67/src/rtp/aes67_xfifo.c
@@ -276,3 +276,20 @@ aes67_fifo_state_t aes67_audio_fifo_get_state(aes67_audio_fifo_t *fifo) {
     else
         return fifo->state;
 }
+
+#define AES67_SENDER_BUFFER_LEN                                                \
+    (AES67_MAX_CHANNELS_PER_SENDER * MAX_SAMPLES_PER_RTP_PACKET)
+
+// Double buffers for RTP sender - moved here to avoid XC aliasing issues
+static uint32_t tx_buffer0[NUM_AES67_SENDERS][AES67_SENDER_BUFFER_LEN];
+static uint32_t tx_buffer1[NUM_AES67_SENDERS][AES67_SENDER_BUFFER_LEN];
+
+uint32_t *aes67_get_tx_buffer0(int32_t sender_id) {
+    assert(is_valid_sender_id(sender_id));
+    return tx_buffer0[sender_id];
+}
+
+uint32_t *aes67_get_tx_buffer1(int32_t sender_id) {
+    assert(is_valid_sender_id(sender_id));
+    return tx_buffer1[sender_id];
+}
